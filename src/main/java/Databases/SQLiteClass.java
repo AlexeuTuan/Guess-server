82d152fbb5640bcf4fd78ae39606dee682e1d762
Alexeu <tuan.tuan.tuan@mail.ru>
2018-05-13 13:51:17 +0800

    Initial commit

---
+++ src/main/java/Databases/SQLiteClass.java
@@ -0,0 +1,43 @@
+package Databases;
+
+import javax.naming.NamingException;
+import java.sql.*;
+
+public class SQLiteClass {
+    public static Connection conn;
+    public static Statement stat;
+    public static ResultSet rs;
+
+    public static void Conn() throws ClassNotFoundException, SQLException, NamingException {
+        Class.forName("org.sqlite.JDBC");
+        conn = DriverManager.getConnection("jdbc:sqlite:ImagesDB.s3db");
+    }
+
+    public static void addPicture(String arrayPicture) throws SQLException, ClassNotFoundException {
+        try {
+            Conn();
+            stat = conn.createStatement();
+            String[] ss = arrayPicture.split(",");
+            stat.execute("INSERT INTO 'Images'('ImageName','PointsArray') VALUES('" + ss[ss.length] + "','" + arrayPicture + "'");
+
+        } catch (Exception e) {
+            System.out.println(e);
+        } finally {
+            stat.close();
+            CloseDB();
+        }
+    }
+
+    public static String getPicture(int id) throws SQLException, NamingException, ClassNotFoundException {
+        Conn();
+        stat = conn.createStatement();
+        rs = stat.executeQuery("SELECT * FROM 'Images' WHERE '_id' = " + id + ");");
+        return rs.toString();
+    }
+
+    public static void CloseDB() throws ClassNotFoundException, SQLException {
+        conn.close();
+        stat.close();
+        rs.close();
+    }
+}
---
+++ src/main/java/org/mycompany/myname/HelloServlet.java
@@ -0,0 +1,57 @@
+package org.mycompany.myname;
+
+
+import Databases.SQLiteClass;
+
+import javax.naming.NamingException;
+import javax.servlet.ServletException;
+import javax.servlet.http.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.io.UnsupportedEncodingException;
+import java.sql.SQLException;
+
+import static Databases.SQLiteClass.getPicture;
+
+
+public class HelloServlet extends HttpServlet {
+
+    public void doGet(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse) throws IOException {
+        // httpServletResponse.getWriter().print("Hello from servlet");
+
+
+    }
+
+    public void doPost(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
+        StringBuffer jb = new StringBuffer();
+        String line = null;
+        try {
+            BufferedReader reader = req.getReader();
+            while ((line = reader.readLine()) != null)
+                jb.append(line);
+        } catch (Exception e) { /*report an error*/ }
+
+        if(jb.toString().startsWith("1")) {
+            try {
+                SQLiteClass.addPicture(jb.toString());
+            } catch (SQLException e) {
+                e.printStackTrace();
+            } catch (ClassNotFoundException e) {
+                e.printStackTrace();
+            }
+        } else if(jb.toString().startsWith("2")) {
+            try {
+                String ss = SQLiteClass.getPicture( (int) Math.random()*10);
+            } catch (SQLException e) {
+                e.printStackTrace();
+            } catch (NamingException e) {
+                e.printStackTrace();
+            } catch (ClassNotFoundException e) {
+                e.printStackTrace();
+            }
+
+        }
+
+    }
+}
